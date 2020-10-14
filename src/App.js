5c8568b27ead1e1d68b2866d4aa183ac66d92b89
fabio loterio <vitorloterio@gmail.com>
2020-10-13 21:33:03 -0300

    screen design(elements and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const MostraVoltas = (props) =>{
+  return (
+    <p>
+      {props.voltas}<br/>
+      voltas
+    </p>
+  )
+}
+
+const MostraTempo = (props) => {
+  return (
+    <p>
+      {props.tempo}<br/>
+      Tempo médio por volta
+    </p>
+  )
+}
+
+const Btn = (props) => <button>{props.label}</button>
+
+// function plus = () =>{
+
+// }
 
 function App() {
   return (
-    <h1>lets start</h1>
+    <div className='App'>
+      <MostraVoltas voltas='7' />
+      <Btn label='+' />
+      <Btn label='-' />
+      <MostraTempo tempo='01:35' />
+      
+      <Btn label='Iniciar' />
+      <Btn label='Reiniciar' />
+    </div>
   );
 }
 
